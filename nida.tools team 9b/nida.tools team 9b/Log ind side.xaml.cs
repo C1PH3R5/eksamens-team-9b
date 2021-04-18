45673ef0520114187756ae5ce7616058a2754c97
himbaerbrus <70754910+himbaerbrus@users.noreply.github.com>
2021-04-18 13:53:23 +0200

    Ændre icons og tilføjet Logind side WPF

---
+++ nida.tools team 9b/nida.tools team 9b/Log ind side.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace nida.tools_team_9b
+{
+    /// <summary>
+    /// Interaction logic for Log_ind_side.xaml
+    /// </summary>
+    public partial class Log_ind_side : Window
+    {
+        public Log_ind_side()
+        {
+            InitializeComponent();
+        }
+    }
+}
