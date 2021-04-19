0a91a7e1a61e33c2487dff2b0b515d6db916eed2
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2021-04-19 12:27:47 +0200

    datagrid V1.3

---
+++ nida.tools team 9b/nida.tools team 9b/Model/Medarbejder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nida.tools_team_9b.Model
+{
+    class Medarbejder
+    {
+        private string _Navn;
+
+        public string navn
+        {
+            get { return _Navn; }
+            set { _Navn = value; }
+        }
+        private string _Status;
+
+        public string status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        private DateTime _StempleInd;
+
+        public DateTime stempleInd
+        {
+            get { return _StempleInd; }
+            set { _StempleInd = value; }
+        }
+
+
+    }
+}
