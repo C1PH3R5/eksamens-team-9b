98032c13f4a25bfa25f2d8fb974e1f780305db2e
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2021-04-19 14:27:37 +0200

    Datagrid Final push V1.5

---
+++ nida.tools team 9b/nida.tools team 9b/View/page/TimeBank.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nida.tools_team_9b.View.page
+{
+    /// <summary>
+    /// Interaction logic for TimeBank.xaml
+    /// </summary>
+    public partial class TimeBank : Page
+    {
+        public TimeBank()
+        {
+            InitializeComponent();
+        }
+    }
+}
