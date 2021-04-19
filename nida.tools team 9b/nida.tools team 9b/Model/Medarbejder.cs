e4c9cd9b80e25d61a2eba97662787646b2b070c3
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2021-04-19 14:26:55 +0200

    time first push V1

---
+++ nida.tools team 9b/nida.tools team 9b/Model/Medarbejder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nida.tools_team_9b.Model
+{
+    public class Medarbejder
+    {
+        private string _Navn;
+
+        public string navn
+        {
+            get { return _Navn; }
+            set { _Navn = value; }
+        }
+        private string _Status;
+
+        public string status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        private DateTime _StempleInd;
+
+        public DateTime stempleInd
+        {
+            get { return _StempleInd; }
+            set { _StempleInd = value; }
+        }
+        private DateTime _StempleUD;
+
+        public DateTime stempleUd
+        {
+            get { return _StempleUD; }
+            set { _StempleUD = value; }
+        }
+        private string _Kommentar;
+
+        public string kommentar
+        {
+            get { return _Kommentar; }
+            set { _Kommentar = value; }
+        }
+    }
+}
---
+++ nida.tools team 9b/nida.tools team 9b/ViewModel/MødtMedarbejder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using nida.tools_team_9b.Model;
+
+namespace nida.tools_team_9b.ViewModel
+{
+    public class MødtMedarbejder
+    {
+        public static List<Medarbejder> LoadMedarbjderDataSet()
+        {
+            List<Medarbejder> MødtMedarbejder = new List<Medarbejder>();
+            MødtMedarbejder.Add(new Medarbejder()
+            {
+                navn = "Mikkel Christensen",
+                status = "mødt",
+                stempleInd = new DateTime(2021, 04, 01, 7, 0, 0),
+                stempleUd = new DateTime(),
+                kommentar = ""
+            });
+            MødtMedarbejder.Add(new Medarbejder()
+            {
+                navn = "Tor Kristiansen",
+                status = "gået",
+                stempleInd = new DateTime(2021, 04, 01, 7, 0, 0),
+                stempleUd = new DateTime(2021, 04, 01, 12, 0, 0),
+                kommentar = ""
+            });
+
+            return MødtMedarbejder;
+        }
+    }
+}
