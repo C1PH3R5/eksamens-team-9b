f1516aa77bbbbb1a0d8e7b6d18dcd9e692272d1f
himbaerbrus <70754910+himbaerbrus@users.noreply.github.com>
2021-04-20 23:11:30 +0200

    oprettet tjek ind

---
+++ nida.tools team 9b/nida.tools team 9b/Tjek_Ind_ud.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace nida.tools_team_9b
+{
+    /// <summary>
+    /// Interaction logic for Tjek_Ind_ud.xaml
+    /// </summary>
+    public partial class Tjek_Ind_ud : Window
+    {
+        public Tjek_Ind_ud()
+        {
+            InitializeComponent();
+        }
+    }
+}
