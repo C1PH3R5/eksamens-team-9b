d6dc89bfe6b8b4f5ed597f4eae8fa23d5a200ce8
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2021-04-28 10:11:49 +0200

    DbCon fil upload

---
+++ nida.tools team 9b/nida.tools team 9b/Database/DBCon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace nida.tools_team_9b.Database
+{
+    class DBCon
+    {
+        protected MySqlConnection GetConnection()
+        {
+            StringBuilder connString = new StringBuilder();
+            connString.Append("Server=localhost;");
+            connString.Append("Port=3306;");
+            connString.Append("Database=NidaTools;");
+            connString.Append("Uid=nidatools;");
+            connString.Append("password=nidatools;");
+            return new MySqlConnection(connString.ToString());
+        }
+    }
+    
+}
