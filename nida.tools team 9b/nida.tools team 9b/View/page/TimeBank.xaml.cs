9a0c77fb9ec803d168ec5f7126fcd5db7bd66172
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2021-05-11 12:46:12 +0200

    Revert "deletet usercontrolls from branch dynamisk menu"
    This reverts commit 1d0767415a625e55227f8e2ae3d45276a3f3f7c9.

---
+++ nida.tools team 9b/nida.tools team 9b/View/page/TimeBank.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using nida.tools_team_9b.ViewModel;
+
+namespace nida.tools_team_9b.View.page
+{
+    /// <summary>
+    /// Interaction logic for TimeBank.xaml
+    /// </summary>
+    public partial class TimeBank : Page
+    {
+        public TimeBank()
+        {
+            InitializeComponent();
+        }
+        private void Søg_Clicked(object sender, RoutedEventArgs e)
+        {
+
+            string til = Convert.ToDateTime(tilDato.SelectedDate).ToString("yyyy/MM/dd") + " 00:00:00";
+            string fra = Convert.ToDateTime(fraDato.SelectedDate).ToString("yyyy/MM/dd") + " 00:00:00";
+            ArbjedesTimer.ItemsSource = TimeBankViewModel.GetTimeBankList(til, fra);
+            //ArbjedesTimer.ItemsSource =  TimeBankViewModel.getTimeBankList(fraDato.SelectedDate.ToString(), tilDato.SelectedDate.ToString());
+        }
+
+        private void expoter_Clicked(object sender, RoutedEventArgs e)
+        {
+            string til = Convert.ToDateTime(tilDato.SelectedDate).ToString("yyyy/MM/dd") + " 00:00:00";
+            string fra = Convert.ToDateTime(fraDato.SelectedDate).ToString("yyyy/MM/dd") + " 00:00:00";
+            TimeBankViewModel.CsvFileWriter(til, fra);
+        }
+    }
+}
---
+++ nida.tools team 9b/nida.tools team 9b/View/page/Tjek_ind_ud.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nida.tools_team_9b.View.page
+{
+    /// <summary>
+    /// Interaction logic for Tjek_ind_ud.xaml
+    /// </summary>
+    public partial class Tjek_ind_ud : Page
+    {
+        public Tjek_ind_ud()
+        {
+            InitializeComponent();
+        }
+    }
+}
